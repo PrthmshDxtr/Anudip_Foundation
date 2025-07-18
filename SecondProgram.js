3207bb323e8951c8ba6b15992d37d8e38f9c811b
PrthmshDxtr <160320888+PrthmshDxtr@users.noreply.github.com>
2025-07-18 21:57:10 +0530

    Add files via upload

---
+++ SecondProgram.js
@@ -0,0 +1,7 @@
+let one = "this is first sentence";
+let two = "thsi is second sentence";
+let three ="this is third sentence";
+
+console.log("Characters in first sentence:",one.length);
+console.log("Characters in second sentence:",two.length);
+console.log("Characters in third sentence:",three.length);
